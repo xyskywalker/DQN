f5a3b46f84d5c0a8cf1384a705d1d97b896b5b7c
Xing Yi <xyplus@live.com>
2017-07-25 08:49:59 +0800

    Update

--- PRJ_1/train_test.py
+++ PRJ_1/train_test.py
@@ -3,14 +3,13 @@ import numpy as np
 import pandas as pd
 import datetime
 import os
+import tensorflow as tf
+import tensorflow.contrib as tfc
 
-time_start = datetime.datetime(year=2017, month=7, day=29)
-time_end = datetime.datetime(year=2016, month=5, day=31)
 
-print(time_start.day)
-print(time_start.weekday())
-i = 2 if ((2 == 1) | (2 == 2)) else 1
-print(i)
-#time_start += datetime.timedelta(days=300)
-#if time_start >= time_end:
-#    print('>')
+train_data = np.load('train_data.npy')
+
+print(train_data.shape)
+print(train_data[0])
+print(train_data[0].shape)
+
