f02d23d25930fd6e8740e56bd129b49be07f4345
Xing Yi <xyplus@live.com>
2017-08-07 15:18:14 +0800

    Update

--- PRJ_0/DataReader.py
+++ PRJ_0/DataReader.py
@@ -4,23 +4,24 @@ import pandas as pd
 import datetime
 
 
-#print(datetime.datetime.now(), 'Start - Load Data')
-#csv_train = pd.read_csv('df_train.csv').fillna(value=0.0)
-#print(datetime.datetime.now(), 'End - Load Data')
-#csv_train.drop(['交易时间','申报受理时间', '住院开始时间','住院终止时间','操作时间','出院诊断病种名称'], axis=1, inplace=True)
+print(datetime.datetime.now(), 'Start - Load Data')
+csv_train = pd.read_csv('df_test.csv').fillna(value=0.0)
+print(datetime.datetime.now(), 'End - Load Data')
+csv_train.drop(['交易时间','申报受理时间', '住院开始时间','住院终止时间','操作时间','出院诊断病种名称'], axis=1, inplace=True)
 
-#np.save('arr_train.npy', csv_train)
-#print(datetime.datetime.now(), 'Start - Load Data')
-#arr_train = np.load('arr_train.npy')
+np.save('arr_test.npy', csv_train)
+print(datetime.datetime.now(), 'Start - Load Data')
+arr_train = np.load('arr_test.npy')
 #train_data = np.load('/media/xy/247E930D7E92D740/ShareData/train_data.npy')
-#print(datetime.datetime.now(), 'End - Load Data')
+print(datetime.datetime.now(), 'End - Load Data')
+
+#df_test = pd.DataFrame(arr_train)
+
+#print(df_test.groupby(by=1)[1].count().max()) #Test Data: 907
+
 #print(train_data.shape)
 #print(train_data[0].shape)
 #print(train_data[0])
-
-arr1 = np.array([1,1,0,0,0,1,0])
-arr2 = np.array([1,1,0,0,0,0,0])
-print(sum(arr1))
 
 #print(arr_train)
 #print(arr_train[0])
@@ -29,30 +30,28 @@ print(sum(arr1))
 #print(arr_train[0][2])
 #print(arr_train[0][3])
 #print(datetime.datetime.now(), 'Start - Load Data')
-#df_train = pd.DataFrame(arr_train)
-#df_label = pd.read_csv('df_id_train.csv',header=-1, encoding='utf-8')
-#arr_label = np.array(df_label)
+df_train = pd.DataFrame(arr_train)
+df_label = pd.read_csv('df_id_test.csv',header=-1, encoding='utf-8')
+arr_label = np.array(df_label)
 
-#train_list = list(range(len(arr_label)))
+train_list = list(range(len(arr_label)))
 
 #print(np.array(df_train[df_train[1]==352121004173837].drop([0, 1], axis=1)).shape)
-#for i in range(20000):
-#    item_arr = np.zeros([1415, 61])
+for i in range(4000):
+    item_arr = np.zeros([1415, 61])
     #print(df_train[df_train[1]==arr_label[i][0]].sort_values(by=0, ascending=True))
     #print(df_train[df_train[1]==arr_label[i][0]].sort_values(by=0, ascending=True).drop([0, 1], axis=1))
-#    item_temp = np.array(df_train[df_train[1]==arr_label[i][0]].sort_values(by=0, ascending=True).drop([0, 1], axis=1))
-#    item_arr[0:len(item_temp), ] = item_temp
+    item_temp = np.array(df_train[df_train[1]==arr_label[i][0]].sort_values(by=0, ascending=True).drop([0, 1], axis=1))
+    item_arr[0:len(item_temp), ] = item_temp
     #print(item_temp)
 
-#    train_list[i] = item_arr
+    train_list[i] = item_arr
 
-#    if i % 10 == 0:
-#        print('Steps:', i)
+    if i % 10 == 0:
+        print('Steps:', i)
 
 #print(train_list)
-#np.save('/media/xy/247E930D7E92D740/ShareData/train_data.npy', train_list)
-
-
+np.save('/media/xy/247E930D7E92D740/ShareData/test_data.npy', train_list)
 
 
 #print(datetime.datetime.now(), 'End - Load Data')
