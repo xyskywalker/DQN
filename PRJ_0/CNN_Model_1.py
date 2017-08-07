d387ccf4393646c51efce3b5cf834f63d5a0c6ae
Xing Yi <xyplus@live.com>
2017-08-07 13:50:03 +0800

    Update

--- PRJ_0/CNN_Model_1.py
+++ PRJ_0/CNN_Model_1.py
@@ -1,16 +1,56 @@
 import numpy as np
 import pandas as pd
 import datetime
+import random
 import tensorflow as tf
 import tensorflow.contrib as tfc
 
 print(datetime.datetime.now(), 'Start - Load Data')
 #train_data = np.load('/media/xy/247E930D7E92D740/ShareData/train_data.npy')
 train_data = np.load('train_data.npy')
+df_label = pd.read_csv('df_id_train.csv',header=-1, encoding='utf-8')
+#arr_label = np.array(df_label)
+id_list_1 = np.array(df_label[df_label[1] == 1].index)
+id_list_0 = np.array(df_label[df_label[1] == 0].index)
 print(datetime.datetime.now(), 'End - Load Data')
 
-df_label = pd.read_csv('df_id_train.csv',header=-1, encoding='utf-8')
-arr_label = np.array(df_label)
+#print(train_data.shape) #(20000, 1415, 61)
+#print(train_data[0].shape) #(1415, 61)
+
+
+def get_data(batch_size = 50, is_train = True):
+    # 1, 0的数据各随机获取 batch_size/2
+    half_batch = int(batch_size/2)
+    if is_train:
+        list_1 = list(id_list_1[0:900])
+        list_0 = list(id_list_0[0:17100])
+    else:
+        list_1 = list(id_list_1[900:])
+        list_0 = list(id_list_0[17100:])
+    id_1 = random.sample(list_1, half_batch)
+    id_0 = random.sample(list_0, half_batch)
+    train_x = np.zeros([50, 1415, 61], dtype=np.float32)
+    train_y = np.zeros([50], dtype=np.int32)
+    # 随机取 batch_size/2 的ID，用来随机交错填入1和0的数据
+    random_ids = random.sample(range(batch_size), half_batch)
+    i_1 = 0
+    i_0 = 0
+    for i in range(batch_size):
+        if i in random_ids:
+            train_x[i] = train_data[id_1[i_1]]
+            train_y[i] = 1
+            i_1 += 1
+        else:
+            train_x[i] = train_data[id_0[i_0]]
+            train_y[i] = 0
+            i_0 += 1
+
+    return train_x, train_y
+
+x, y = get_data()
+#print(x)
+#print(y)
+
 
 def print_activations(t):
     print(t.op.name, '' , t.get_shape().as_list())
@@ -62,77 +102,29 @@ h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob=keep_prob)
 w_actiontype = tf.get_variable('w_actiontype', shape=[1024, 2], initializer=tf.contrib.layers.xavier_initializer())
 b_actiontype = tf.get_variable('b_actiontype', shape=[2], initializer=tf.contrib.layers.xavier_initializer())
 layer_actiontype_p = tf.matmul(h_fc1_drop, w_actiontype) + b_actiontype
-layer_actiontype = tf.nn.softmax(layer_actiontype_p)
-actiontype_output = tf.argmax(layer_actiontype, 1)
 
-y_input = tf.placeholder(shape=[None, 1], dtype=tf.int32)
-y_onehot = tf.one_hot(y_input, depth=2)
-
-y_o = tf.placeholder(shape=[None], dtype=tf.int64)
-
-cross_count = tf.cast(tf.reduce_sum(tf.multiply(actiontype_output, y_o)), dtype=tf.float32)
-cross_zero = tf.greater(cross_count, 0.0)
-
-def fn1():
-    return tf.constant(0.0, dtype=tf.float32)
-
-def fn2():
-    return tf.constant(1.0, dtype=tf.float32)
-
-f1_ = tf.cond(cross_zero, fn1, fn2, name='f1_')
-
-prediction_count = tf.cast(tf.reduce_sum(actiontype_output), dtype=tf.float32)
-reference_count = tf.cast(tf.reduce_sum(y_o), dtype=tf.float32)
-
-precision = tf.div(cross_count, prediction_count + tf.cond(tf.greater(prediction_count, 0.0), fn1, fn2))
-recall = tf.div(cross_count, reference_count + tf.cond(tf.greater(reference_count, 0.0), fn1, fn2))
-
-f1 = tf.div(tf.multiply(2.0, tf.multiply(precision, recall)), tf.add(tf.add(precision, recall), f1_))
-
-
+y_input = tf.placeholder(shape=[None], dtype=tf.int32)
+y_ = tf.reshape(y_input, shape=[-1, 1])
+y_onehot = tf.one_hot(y_, depth=2)
 
 # 成本函数 reduce mean 降维->平均值
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=layer_actiontype_p, labels=y_onehot))
 # 使用了Adam算法来最小化成本函数
-cost2 = (1 - f1) * cost * 10
-optimizer2 = tf.train.AdamOptimizer(learning_rate).minimize(cost2)
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-
-print_activations(envInput)
-print_activations(envIn)
-print_activations(conv1)
-print_activations(conv2)
-print_activations(conv3)
-print_activations(pool1)
-print_activations(h_fc1)
-print_activations(layer_actiontype_p)
 
 with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
+    cost_all = 0.0
     for e in range(10000):
-        cost_all = 0.0
-        accuracy_all = 0.00
-        for i in range(18):
-            i_start = 1000 * i
-            i_end = 1000 * i + 1000
-            xs = train_data[i_start:i_end]
-            y_ = arr_label[i_start:i_end, 1]
-            ys = np.reshape(y_, [-1, 1])
+        xs, ys = get_data()
+        cost_, _ = sess.run([cost, optimizer], feed_dict={envInput: xs, y_input: ys, keep_prob: 0.5})
+        cost_all += cost_
+        if e % 10 == 0:
+            print('Steps: ', e, 'Cost:', cost_all / 10)
+            xs, ys = get_data(batch_size=50, is_train=False)
+            cost_, _ = sess.run([cost, optimizer],
+                                                   feed_dict={envInput: xs, y_input: ys, keep_prob: 1})
+            print('Test Cost:', cost_)
+            cost_all = 0.0
 
-            o1_, o2_,cross_count_, f1_ = sess.run([cost2, optimizer2, cross_count, f1], feed_dict={envInput: xs, y_input: ys, keep_prob: 0.1, y_o: y_})
-            cost_all += o1_
-            print('Cost:', o1_, 'F1', f1_, 'cross_count', cross_count_)
-
-        cost_all = cost_all/18.0
-        accuracy_all = accuracy_all/18.0
-
-        i_start = 18000
-        i_end = i_start + 2000
-        xs = train_data[i_start:i_end]
-        y_ = arr_label[i_start:i_end, 1]
-        ys = np.reshape(y_, [-1, 1])
-        print('e', e, 'cost', cost_all)
-
-        t_, o1_, o3_, cross_count_ = sess.run([precision, recall, f1, cross_count], feed_dict={envInput: xs, y_input: ys, keep_prob: 1.0, y_o: y_})
-        print('precision: ', t_, ' recall: ', o1_, ' F1: ', o3_, 'cross_count', cross_count_)
